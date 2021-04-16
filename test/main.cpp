d72a5d11e7e7fc2bb409bee74ac58f0e0673436d
Jisu Sim <rlj1202@gmail.com>
2021-04-16 14:24:48 +0000

    Working testing code

--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,25 @@
 #include <cstdio>
 
+#include <pugixml.hpp>
+
+#ifdef EMSCRIPTEN
+#include <emscripten.h>
+#endif
+
 using namespace std;
 
 int main() {
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("../../res/block_types.xml");
+    if (!result) {
+        printf("Failed to load xml file\n");
+        return -1;
+    }
+    
+    for (pugi::xml_node block_type : doc.child("Config").child("BlockTypes").children("BlockType")) {
+        printf("block type name : %s\n", block_type.child("Name").child_value());
+    }
+
     printf("test...\n");
 
     return 0;
